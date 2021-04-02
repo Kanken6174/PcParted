6ed49d1fdfb4d4ba12fc13cca6c7860fccd98dfa
Kanken6174 <yorick.geoffre@orange.fr>
2021-04-02 23:04:03 +0200

    modified some stuff

--- src/obj/Debug-git/PcParted_Content.g.i.cs
+++ src/obj/Debug-git/PcParted_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("gpu1.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("gpu-mining.ico")]
 
 
